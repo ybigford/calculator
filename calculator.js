6c022c624277153a800a7f152e71fa825a12494c
Yapa Bigford <110362211+ybigford@users.noreply.github.com>
2022-09-04 13:20:57 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -3,6 +3,7 @@
     let buttons = document.querySelectorAll('.btn');
     let clear = document.querySelector('.btn-clear');
     let equal = document.querySelector('.btn-equal');
+    let del = document.querySelector('.btn-del');
 
 
     buttons.forEach(function(button){
@@ -25,6 +26,11 @@
     clear.addEventListener('click', function(e) {
         screen.value = '';
         
+    })
+    
+    
+    del.addEventListener('click', function(e) {
+        screen.value = screen.value.slice(0, -1);
     })
 
 
